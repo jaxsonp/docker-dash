354537043f916ec65b02d82e5cc56e0f9c1c5314
JaydenPahukula <jayden.pahukula@gmail.com>
2023-06-28 21:16:54 -0400

    swarmify getAppStatus

--- methods/swarmGetNodeInfo.py
+++ methods/swarmGetNodeInfo.py
@@ -3,7 +3,7 @@ import flask
 from . import internal_methods
 
 @internal_methods.verifyFacilityID
-@internal_methods.verifyDockerEngine(swarm_method=False)
+@internal_methods.verifyDockerEngine(swarm_method=True)
 def swarmGetNodeInfo(facility_id, app_name="", app_id="") -> flask.Response:
   """
   Returns information on specified node
