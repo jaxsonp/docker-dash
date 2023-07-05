ad63a92236daf6ca669cd290dbfcc53015f6eb25
server4 <server4@localhost.server4>
2023-07-05 11:40:12 -1000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# checking if dir exists
+if [ -d "src-container-api/" ]; then
+  # prompting yes or no for overwrite
+  while true; do
+    read -p "Directory \"src-container-api/\" already exists, overwrite? (y/n) " yn
+    case $yn in 
+      [yY] ) 
+        break
+        ;;
+      [nN] ) 
+        echo Exiting...; exit
+        ;;
+      * ) 
+        echo invalid response
+        ;;
+    esac
+  done
+fi
 # verifying sudo
 sudo true
 
@@ -10,18 +28,17 @@ echo done
 
 # extracting tar archive
 echo -n "Extracting... "
-rm -rf src-container-api/
+sudo rm -rf src-container-api/
 mkdir src-container-api/
-tar -sxf ./src-container-api.tar -C ./src-container-api/ --strip-components=1 > /dev/null 2>&1
-rm src-container-api.tar
+tar -sxf ./src-container-api.tar -C ./src-container-api/ --strip-components=1 &> /dev/null
+sudo rm src-container-api.tar
 echo done
 
 
 # checking for docker
 echo -n "Verifying docker... "
-sudo docker ps > /dev/null 2>&1
-if [ $? -gt 0 ]
-then
+sudo docker ps &> /dev/null
+if [ $? -gt 0 ]; then
   echo "not found"
   # prompting yes or no for docker installation
   while true; do
@@ -41,23 +58,22 @@ then
 
   # installing docker
   echo -n "Installing docker... "
-  sudo yum install -y yum-utils > /dev/null
-  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null
-  sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
+  sudo yum install -y yum-utils &> /dev/null
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &> /dev/null
+  sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &> /dev/null
 fi
 echo done
 
 # configuring docker
 echo -n "Configuring docker... "
 sudo systemctl start docker
-sudo systemctl enable docker > /dev/null
+sudo systemctl enable docker &> /dev/null
 sudo usermod -aG docker $USER
 echo done
 
 # verifying python
 echo -n "Verifying python... "
-if [ "$(python3.9 --version 2>&1)" != "Python 3.9.16" ]
-then
+if [ "$(python3.9 --version 2>&1)" != "Python 3.9.16" ]; then
   echo "not found"
   # prompting yes or no for python installation
   while true; do
@@ -77,15 +93,15 @@ then
 
   # installing python
   echo -n "Installing python (this may take a few minutes)... "
-  sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel zlib-devel > /dev/null
-  curl -sOL https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz > /dev/null
-  tar -xzf Python-3.9.16.tgz > /dev/null
+  sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel zlib-devel &> /dev/null
+  curl -sOL https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tgz &> /dev/null
+  tar -xzf Python-3.9.16.tgz &> /dev/null
   cd Python-3.9.16 
-  sudo ./configure --enable-optimizations > /dev/null
-  sudo make altinstall > /dev/null
+  sudo ./configure --enable-optimizations &> /dev/null
+  sudo make altinstall &> /dev/null
   cd ..
-  rm Python-3.9.16.tgz
-  rm -rf Python-3.9.16
+  sudo rm Python-3.9.16.tgz
+  sudo rm -rf Python-3.9.16
 fi
 echo done
 
