3b67ee0922ca7dbdb97fec802350a8e11b16c3ff
Jaxson Pahukula <jaxpahu@gmail.com>
2023-07-05 15:42:37 -1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ then
 
   # installing docker
   echo -n "Installing docker... "
-  yum install -y yum-utils > /dev/null
+  sudo yum install -y yum-utils > /dev/null
   sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null
   sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
 fi
