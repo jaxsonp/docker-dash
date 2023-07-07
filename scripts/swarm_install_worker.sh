09dafea3e13ab6b60aa4eb01cac6df0fb57444e1
Jaxson Pahukula <jaxpahu@gmail.com>
2023-07-06 16:29:42 -1000

    update swarm_install_worker.sh

--- scripts/swarm_install_worker.sh
+++ scripts/swarm_install_worker.sh
@@ -52,7 +52,7 @@ echo done
 
 # joining swarm
 echo -n "Joining swarm... "
-sudo docker swarm join --token $token $ip:2377
+sudo docker swarm join --token $token $ip
 
 echo -e "\nTo complete installation, a restart is required"
 # prompting yes or no for restart
