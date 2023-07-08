5f0964c97fa0647d0f5d94aa7803050e04dff558
Jaxson Pahukula <jaxpahu@gmail.com>
2023-07-07 14:59:16 -1000

    add get-users

---
+++ methods/solo/soloGetUsers.py
@@ -0,0 +1,26 @@
+import json
+import flask
+from methods import internal_methods
+
+
+@internal_methods.verifyFacilityID
+@internal_methods.verifyDockerEngine(swarm_method=False)
+def soloGetUsers(facility_id) -> flask.Response:
+  """
+  Returns an array of all apps, running or not
+
+  parameters:
+    facility_id - this value is passed in the API route, for demo purposes this should always be "demo"
+  """
+
+  # executing system command
+  completedProcess = internal_methods.subprocessRun(f"docker ps -a --format \"{{{{.Names}}}}\"")
+  if completedProcess.returncode != 0:
+    return flask.make_response("Unknown error:\n"+completedProcess.stdout.decode()+"\n"+completedProcess.stderr.decode(), 500)
+
+  arr = []
+  for string in completedProcess.stdout.decode().split("\n"):
+    if string != "":
+      arr.append(string.split("--")[1])
+
+  return flask.make_response(json.dumps(arr), 200)
---
+++ methods/swarm/swarmGetUsers.py
@@ -0,0 +1,23 @@
+import json
+import flask
+from methods import internal_methods
+
+
+@internal_methods.verifyFacilityID
+@internal_methods.verifyDockerEngine(swarm_method=True)
+def swarmGetUsers(facility_id) -> flask.Response:
+  """
+  Returns an array of all running apps
+
+  parameters:
+    facility_id - this value is passed in the API route, for demo purposes this should always be "demo"
+  """
+
+  # executing system command
+  completedProcess = internal_methods.subprocessRun(f"docker service ls --format \"{{{{.Name}}}}\"")
+  if completedProcess.returncode != 0:
+    return flask.make_response("Unknown error:\n"+completedProcess.stdout.decode()+"\n"+completedProcess.stderr.decode(), 500)
+
+  arr = [s.split("--")[1] for s in completedProcess.stdout.decode().split("\n") if s != ""]
+
+  return flask.make_response(json.dumps(arr), 200)
