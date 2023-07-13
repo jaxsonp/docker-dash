07eeb6c4777519799b6d5a72595681c9af33ba2a
balderekjk <bal.derek.j@gmail.com>
2023-07-13 10:35:18 -1000

    rv1.13

--- client/src/handleFetch.jsx
+++ client/src/handleFetch.jsx
@@ -6,8 +6,8 @@ export default async function handleFetch(name, api) {
       let response = await fetch(api);
       response = await response.json();
       sessionStorage.setItem(name, JSON.stringify(response));
-      JSON.parse(sessionStorage.getItem(name));
       alert("Your request was successed");
+      return JSON.parse(sessionStorage.getItem(name));
     } catch (err) {
       alert(err);
       throw new Error(err);
